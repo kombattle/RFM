6151b28227db6143625edbe94372386dbd16af2c
Ginna Phanvimolchai <88496886+kombattle@users.noreply.github.com>
2022-08-16 11:52:52 +0700

    Create Recency and Monetary
    create Recency by change datediff to amount of day
    and group the sum price by amount*single_price to create Monetary column

--- RFM.R
+++ RFM.R
@@ -5,3 +5,12 @@ library("readxl")
 sale_data <- read_xlsx("sale_data.xlsx")
 
 glimpse(sale_data)
+
+
+new_data <- sale_data %>%
+  mutate(Recency = difftime("2008-12-31",sale_data$date , units = "days")) %>%
+  group_by(customer_id) %>%
+  mutate(Monetary = sum(amount * single_price)) %>% 
+  distinct(customer_id, .keep_all= TRUE)
+
+view(new_data)
