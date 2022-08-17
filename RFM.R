8333ffe0974cb7096fae85b54660ceef2fb7c1f4
Ginna Phanvimolchai <88496886+kombattle@users.noreply.github.com>
2022-08-18 00:37:38 +0700

    update description

--- RFM.R
+++ RFM.R
@@ -1,3 +1,5 @@
+###install packages and library to read xlsx file
+
 install.packages("tidyverse")
 library("tidyverse")
 library("readxl")
@@ -6,7 +8,7 @@ sale_data <- read_xlsx("sale_data.xlsx")
 
 glimpse(sale_data)
 
-
+###tranform date diff to amount of day and sum the amount of price 
 new_data <- sale_data %>%
   mutate(Recency = difftime("2008-12-31",sale_data$date , units = "days")) %>%
   group_by(customer_id) %>%
@@ -15,13 +17,18 @@ new_data <- sale_data %>%
 
 view(new_data)
 
+###count the frequency amount
 fr_data <- sale_data %>%
   count(customer_id)
 
+
+###join data together
 com_data <- fr_data %>%
   left_join(new_data,by = "customer_id") %>%
   select(customer_id, Recency, Frequency = n,Monetary )
 
+
+###clean data Recency to be integer and round number of Monetary
 com_data$Recency <- gsub("[a-z]$","",com_data$Recency) 
 com_data$Recency <- as.integer(com_data$Recency)
 com_data$Monetary <- round(com_data$Monetary,2)
@@ -29,15 +36,9 @@ glimpse(com_data)
 
 view(com_data)
 
-com_data %>%
-  summarise(max_recency = max(Recency),
-            max_monetry = max(Monetary),
-            max_fr = max(frequency),
-            min_fr = min(frequency),
-            min_mon = min(Monetary),
-            min_re = min(Recency))
+summary(com_data)
 
-
+###equal the group of data to 1-5 by arrage
 pre_RFM <- com_data %>%
   mutate(final_R = cut_number(com_data$Recency, n = 5, label = FALSE, binType = "explicit")) %>% 
   mutate(final_F = cut_number(com_data$Frequency, n = 5, label = FALSE, binType = "explicit")) %>%
@@ -46,11 +47,14 @@ pre_RFM <- com_data %>%
 glimpse(pre_RFM)
 view(pre_RFM)
 
+###make number to use with RFM model
 pre_RFM$final_R = 100*(6-pre_RFM$final_R)
 pre_RFM$final_F = 10*pre_RFM$final_F
 
-glimpse(pre_RFM)
+glimpse(pre_RFM)+
 
+
+###create columns to make final columns of RFM model
 final_RFM <- pre_RFM %>%
   mutate(RFM = pre_RFM$final_R+pre_RFM$final_F+pre_RFM$final_M)
 
